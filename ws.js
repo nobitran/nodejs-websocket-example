452ff86d925aa60d318162b9acad8d68349aea84
Thuc Tran <thuc.tv@techchain.vn>
2022-08-29 16:37:12 +0700

    feat: update nodemon

--- ws.js
+++ ws.js
@@ -1,13 +1,12 @@
-var WebSocketServer = require('ws').Server,
-  wss = new WebSocketServer({port: 40510})
+const WebSocketServer = require('ws').Server,
+  wss = new WebSocketServer({ port: 40510 });
 
 wss.on('connection', function (ws) {
   ws.on('message', function (message) {
-    console.log('received: %s', message)
-  })
+    console.log('received: %s', message);
+  });
 
-  setInterval(
-    () => ws.send(`${new Date()}`),
-    1000
-  )
-})
+  ws.send(`${new Date()}`, function (err) {
+    console.log(err);
+  });
+});
