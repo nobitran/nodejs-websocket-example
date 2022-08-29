4dccca83da59835483b572c17c808b3a42ffbb09
Thuc Tran <thuc.tv@techchain.vn>
2022-08-29 16:51:30 +0700

    feat: update port

--- ws.js
+++ ws.js
@@ -1,5 +1,6 @@
 const WebSocketServer = require('ws').Server,
   wss = new WebSocketServer({ port: 40510 });
+console.log('ws://localhost:40510');
 
 wss.on('connection', function (ws) {
   ws.on('message', function (message) {
