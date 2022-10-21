4fdbf7e33405a60755e1919f8a8341c146e539b7
Thuc Tran <thuc.tv@techchain.vn>
2022-10-21 15:52:53 +0700

    feat: move to script groovy

--- script.groovy
+++ script.groovy
@@ -8,18 +8,22 @@ def deployApp() {
 
 def buildImage(imageName, version, creId) {
   echo "Building app ... with $BRANCH_NAME"
-  withCredentials([
-      usernamePassword(
-          credentialsId: creId,
-          usernameVariable: 'USERNAME',
-          passwordVariable: 'PASSWORD'
-      )
-  ]) {
-      sh "docker build -t $imageName ."
-      sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-      sh "docker push $imageName:$version.$BUILD_NUMBER"
-      echo "Pushed image to dockerhub"
-  }
+  def version_name = "$imageName:$version.$BUILD_NUMBER"
+  echo "Version: $version_name"
+  def matcher = readFile('package.json') =~ '"version": "1.0.0"'
+  echo matcher;
+  // withCredentials([
+  //     usernamePassword(
+  //         credentialsId: creId,
+  //         usernameVariable: 'USERNAME',
+  //         passwordVariable: 'PASSWORD'
+  //     )
+  // ]) {
+  //     sh "docker build -t $imageName ."
+  //     sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
+  //     sh "docker push"
+  //     echo "Pushed image to dockerhub"
+  // }
 }
 
 
