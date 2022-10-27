38eaa633355de32e0c2f0fa7306aefb22c6e2520
Thuc Tran <thuc.tv@techchain.vn>
2022-10-27 23:33:45 +0700

    feat: update logic build image

--- script.groovy
+++ script.groovy
@@ -29,19 +29,15 @@ def deployApp() {
   }
 }
 
-def updateVersion(imageName, creId) {
-  echo "imageName: $imageName"
+def updateVersion(image_name, cre_id) {
   echo "Building app ... with $BRANCH_NAME"
   def matcher = readJSON file: 'package.json'
   def new_version = increaseVersion(matcher.version, 'major')
   matcher.version = new_version;
 
   writeJSON file: 'package.json', json: matcher.toString(), pretty: 2
-
   def latest_matcher = readJSON file: 'package.json'
-  echo latest_matcher.version;
-  def new_name = imageName + ':' + latest_matcher.version
-  buildImage(new_name, creId)
+  buildImage(image_name, latest_matcher.version, cre_id)
   commitVersion(latest_matcher.version)
 }
 
